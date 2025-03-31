a8b99a4d8960f542620dcf98e31b2124a69799b1
Kyle Nel <kylenel46@gmail.com>
2025-04-01 00:26:37 +0200

    - Added Second Page - Changed Some xml id declarations - Changed background of 1st page in the process

--- app/src/main/java/com/example/budgetpiggy/WelcomePage2.kt
+++ app/src/main/java/com/example/budgetpiggy/WelcomePage2.kt
@@ -6,23 +6,24 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-
-class MainActivity : AppCompatActivity() {
+class WelcomePage2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.welcome_page_2)
 
-        val nextButton: Button = findViewById(R.id.nextButton)
+        val previousButton: Button = findViewById(R.id.previousButton)
 
-        nextButton.setOnClickListener {
-               val intent = Intent(this, WelcomePage2::class.java)
+        previousButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+
+            // ✅ Apply slide reverse animation
             val options = ActivityOptions.makeCustomAnimation(
                 this,
                 R.anim.fade_in,
                 R.anim.fade_out
             )
-            startActivity(intent, options.toBundle())
 
+            startActivity(intent, options.toBundle())
         }
     }
 }
