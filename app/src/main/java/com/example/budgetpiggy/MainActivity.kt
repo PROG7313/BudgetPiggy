2d0f25a9c00e895c76bae9db4925e98399b642c3
Derik Korf <derikkorf10@gmail.com>
2025-04-01 11:31:34 +0200

    - Removed extra SetContentViews

--- app/src/main/java/com/example/budgetpiggy/MainActivity.kt
+++ app/src/main/java/com/example/budgetpiggy/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
