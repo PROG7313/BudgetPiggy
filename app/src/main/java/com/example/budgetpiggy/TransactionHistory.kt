2fc498afe9ca9f89b3e7eb2d43cebe9470a24999
Derik Korf <derikkorf10@gmail.com>
2025-04-07 11:06:10 +0200

    - Added navbar - Added popup menu - requires actions for hot streak, navbar

--- app/src/main/java/com/example/budgetpiggy/TransactionHistory.kt
+++ app/src/main/java/com/example/budgetpiggy/TransactionHistory.kt
@@ -2,7 +2,6 @@ package com.example.budgetpiggy
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
